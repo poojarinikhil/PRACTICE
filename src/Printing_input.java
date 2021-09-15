7d9305336e032b1ff4d6f941ae388c6d2d26f39f
Nikhil <poojarinikhil01@gmail.com>
2021-09-15 23:59:09 +0530

    For 100 day challege

---
+++ src/Printing_input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Printing_input {
+        public static void main(String[] args){
+            System.out.println("Enter your name :");
+            Scanner sc = new Scanner(System.in);
+            String name =sc.next();
+            System.out.println("hello "+ name + " have a nice day !!");
+        }
+    }
+
