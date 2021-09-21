bcebf51c3624317497f9e7986dd276f1fd29e014
Nikhil <poojarinikhil01@gmail.com>
2021-09-21 22:53:01 +0530

    For 100 day challege

---
+++ src/IF_ELSE_USAGE.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IF_ELSE_USAGE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age :");
+        int age = sc.nextInt();
+        if (age > 18){
+            System.out.println("You can drive");
+        }
+        else {
+            System.out.println("You cannot drive");
+
+        }
+    }
+}
