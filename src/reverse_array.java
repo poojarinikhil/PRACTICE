4f943fd2e356eae8f88d753b91beefa646793678
Nikhil <poojarinikhil01@gmail.com>
2021-10-06 21:11:53 +0530

    reversing an array

---
+++ src/reverse_array.java
@@ -0,0 +1,15 @@
+public class reverse_array {
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5,6,7};
+        int l= arr.length/2;
+        int n=arr.length;
+        for (int i=0;i<l;i++){
+            int temp=arr[(n-i-1)];
+            arr[(n-i-1)] =arr[i];
+            arr[i]=temp;
+        }
+        for (int element : arr){
+            System.out.print(element+" ");
+        }
+    }
+}
