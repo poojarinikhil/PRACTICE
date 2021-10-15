ba7a80cb921e6a01638a51af5b7d49f43617d671
Nikhil <poojarinikhil01@gmail.com>
2021-10-15 14:24:56 +0530

    inherintance in nutshell

--- src/species.java
+++ src/species.java
@@ -8,7 +8,7 @@ public class species {
     public static void main(String[] args) {
         animal a = new animal();
         dog b = new dog();
-        System.out.println(b.a);
+        System.out.println(b.a);    // you can excess a using dog class
         System.out.println(b.b);
         System.out.println(a.a);
     }
