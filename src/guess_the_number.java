6d09f0c8bcd8912b0216abdfd09b6905a435fb90
Nikhil <poojarinikhil01@gmail.com>
2021-10-18 12:46:32 +0530

    guess the number game using oop

--- src/guess_the_number.java
+++ src/guess_the_number.java
@@ -3,36 +3,40 @@ import java.util.Scanner;
 
 class game {
     private int count;
-    int x;
     int a;
+    public int x;
 
     public game() {
         Random random = new Random();
-        int x = random.nextInt(100);
+        this.x = random.nextInt(100);
     }
 
-    int TakeUserInput(int mynumber) {
+    int TakeUserInput() {
+        System.out.println("Guess the correct number ");
         Scanner ab = new Scanner(System.in);
         a = ab.nextInt();
-        return mynumber;
+        return a;
     }
 
-    boolean correct_number(int num1, int num2) {
-        if (num1 > num2) {
+    boolean correct_number(int num) {
+
+        if (num > x) {
             System.out.println("Enter the lesser number");
             count++;
             return false;
         }
-        if (num1 < num2) {
+        else if (num < x) {
             System.out.println("Enter the greater number");
             count++;
             return false;
-        } else {
-            System.out.println("The number guessed is "+x);
+        }
+        else {
+            System.out.println("The right guessed number is " + num);
             return true;
         }
     }
     public int getCount() {
+        System.out.println(" The number of guess are "+count );
         return count;
     }
     public void setCount(int count) {
@@ -42,14 +46,18 @@ class game {
     public class guess_the_number {
 
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
             game gg = new game();
-            gg.setCount(0);
-            while (gg.correct_number(gg.x,gg.a)) {
-                  gg.TakeUserInput(gg.a);
-                System.out.println("The number of guess is : " + gg.getCount());
-                }
-
+            boolean b= false;
+            int i = 0;
+            while (!b){
+                gg.TakeUserInput();
+                b = gg.correct_number(gg.a);
+                System.out.println(b);
+                i++;
+                gg.setCount(i);
+                gg.getCount();
+            }
+            System.out.println("In "+i+" number of guesses you chosed the right number");
 
         }
 }
